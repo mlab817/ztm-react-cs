011ee862884ce394ca1463980fedc12a5f221e0b
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-04-14 13:39:08 +0800

    finished section 10 by fixing class clashes

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import {Fragment, useContext, useEffect, useState} from "react";
 import { useParams } from "react-router-dom";
 
 import {CategoriesContext} from "../../contexts/categories.context";
@@ -16,11 +16,14 @@ const Category = () => {
   }, [category, categoriesMap])
 
   return (
-    <div className="category-container">
-      {
-        products && products.map(product => <ProductCard key={product.id} product={product} />)
-      }
-    </div>
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {
+          products && products.map(product => <ProductCard key={product.id} product={product} />)
+        }
+      </div>
+    </Fragment>
   )
 }
 
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import Button from "../../components/button/button.component";
+import {collection, setDoc} from "firebase/firestore";
+import {db} from "../../utils/firebase/firebase.utils";
 
 const Home = () => {
   const categories = [
@@ -33,11 +35,83 @@ const Home = () => {
     }
   ]
 
+  const zoomApiUrl = 'https://my-zoom-api.herokuapp.com/api/zoom/meetings'
+
+  const createMeeting = async () => {
+    const otherFields = {
+      lastName: 'bolotaolo'
+    }
+
+    const zoomPayload = {
+      topic: 'new meeting',
+      agenda: 'hidden agenda',
+      duration: 60, // in minutes
+      password: '123456',
+      start_time: '2022-04-14T16:00:00',
+      meeting_invitees: [
+        {
+          email: 'mlab817@gmail.com'
+        }
+      ],
+      type: 2 // scheduled
+    }
+
+    try {
+      const response = await axios.post(zoomApiUrl, zoomPayload)
+
+      const { topic,
+        agenda,
+        duration,
+        id,
+        uuid,
+        password,
+        start_time,
+        start_url,
+        join_url,
+        timezone,
+        host_email
+      } = response.data.data // this is the new
+
+      const meetingDetails = {topic,
+        agenda,
+        duration,
+        id,
+        uuid,
+        password,
+        start_time,
+        start_url,
+        join_url,
+        timezone,
+        host_email}
+
+      const fsPayload = {
+        ...meetingDetails,
+        ...otherFields
+      }
+
+      console.log(fsPayload)
+
+      // sendFsPayload to firestore
+
+      const docRef = collection(db, 'meetings') // data.id is the meeting id from zoom or you can use other fields
+      console.log(docRef)
+
+      const fsResponse = await setDoc(docRef, fsPayload)
+
+      console.log(fsResponse)
+
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
   return (
     <>
       <Outlet />
 
       <Directory categories={categories} />
+
+      <button onClick={createMeeting}>create meeting</button>
     </>
   )
 }
